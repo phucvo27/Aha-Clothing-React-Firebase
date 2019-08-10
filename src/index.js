c15d07379b4be2acb29a08fa8629beb138a7c59a
phucvo27 <learninge27@gmail.com>
2019-08-10 14:04:37 +0700

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoute from './Routers/AppRoute.component';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor} from './redux/store';
 
 import './scss/main.scss';
 ReactDOM.render(
     <Provider store={store}>
-        <AppRoute />
+        <PersistGate persistor={persistor}>
+            <AppRoute />
+        </PersistGate>
+        
     </Provider>
     , document.getElementById('root'));
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
@@ -6,7 +7,10 @@ import rootReducer from './root-reducer';
 const middlewares = [logger];
 
 // also do like this :   applyMiddleware(logger);
-const store = createStore(rootReducer, applyMiddleware(...middlewares)); // ...middleware -> will spread all the item into the seperate invidual item 
+export const store = createStore(rootReducer, applyMiddleware(...middlewares)); // ...middleware -> will spread all the item into the seperate invidual item 
 
 // so if : middlewares = [a, b] -> applyMiddleware(...middlewares) <=> applyMiddleware(a, b);
-export default store;
+
+export const persistor = persistStore(store);
+
+//export { store, persistor};
