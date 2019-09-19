6410b8ccb86d157ac87925c1bac88bf9a2977cf2
phucvo27 <learninge27@gmail.com>
2019-09-19 13:41:07 +0700

    preparing for using redux-saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,16 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
-import thunk from 'redux-thunk'
+//import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
-const middlewares = [logger, thunk];
+const sagaMiddleware = createSagaMiddleware();
 
+const middlewares = [sagaMiddleware];
+
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger);
+}
 // also do like this :   applyMiddleware(logger);
 export const store = createStore(rootReducer, applyMiddleware(...middlewares)); // ...middleware -> will spread all the item into the seperate invidual item 
 
